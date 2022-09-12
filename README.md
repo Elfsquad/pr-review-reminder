6438dde0fb2d5784c4a5ff9d20fb51f97d758e63
Stan van Rooy <me@stanvanrooy.com>
2022-09-12 16:43:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Input
+- approval-count
+  - description: The amount of approvals required, before a PR can be merged.
+  - required: true
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const core = require('@actions/core');
+const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_API_URL } = process.env;
+
+const PR_ENDPOINT = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls`;
+const REVIEW_ENDPOINT = `${GITHUB_API_URL}/repos/${GITHUB_REPOSITORY}/pulls/{id}/reviews`;
+
+const approvalCount = core.getInput('approval-count');
+
+const getHeaders = () => {
+  return {
+    Authorization: `token ${GITHUB_TOKEN}`,
+  };
+}
+
+const getPrs = () => {
+  return fetch(PR_ENDPOINT, {
+    method: 'GET',
+    headers: getHeaders()
+  }).then(r => r.json());
+}
+
+const hasReviewers = (pr) => {
+  return pr.requested_reviewers.length > 0 || pr.requested_teams.length > 0;
+}
+
+const getReviews = (pr) => {
+  const url = REVIEW_ENDPOINT.replace('{id}', pr.id);
+  return fetch(url, {
+    method: 'GET',
+    headers: getHeaders()
+  }).then(r => r.json());
+}
+
+const hasEnoughApprovals = async (pr) => {
+  const reviews = await getReviews(pr);
+  return reviews.length >= approvalCount;
+}
+
+const getPrsEligbleForReminder = async (prs) => {
+  const ret = [];
+  const filteredPrs = prs.filter(hasReviewers);
+
+  for(const pr in prs) {
+    if (await hasEnoughApprovals(pr)) {
+      continue;
+    }
+    ret.push(pr);
+  }
+  return ret;
+}
+
+
+(async function () {
+  try {
+    const prs = await getPrs();
+    core.info(`There are total of ${prs.length} prs.`);
+    const prsEligbleForReminder = await getPrsEligbleForReminder(prs.data);
+    core.info(`A total of ${prsEligbleForReminder} are elgible for a reminder.`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
+
